0abfe4fb515902b04ef50b9f6bc0c4e8ca954c3f
handandkeep <1477792904@qq.com>
2024-01-02 19:32:27 +0800

    optimize

--- keyplay/bind.py
+++ keyplay/bind.py
@@ -7,6 +7,13 @@ from keyplay.map import key_all_note as key_note
 from keyplay.play import play_chord
 
 
+"""bind.py
+这个模块监控键盘输入，将输入的键符转换为音符并且合成成一个和弦进行播放,
+其最终能呈现的效果是：当用户写代码或者打字时，会基于其输入的键符生成旋律并播放，
+达到边听音乐编写代码或者是打字的效果，且音乐均基于用户的输入进行合成。
+"""
+
+
 class KeyStack:
     def __init__(self):
         self.stack = []
@@ -86,19 +93,16 @@ def on_release(key):
         rhythm_play()  
     elif key == Key.esc:
         return False
-    else:
-        print("record a key to note")
 
 
-def show_in_terminal():
-    print("Welecome to the keyplay:")
-    print("keyplay aim to play music when you are playing the keyboard")
-    print("it is has defined all the key map:")
-    for k, v in key_note.items():
-        if v != "":
-            print(f"key:{k} => note:{v}")
-    print("when you code or write passage, you can get a rhythm by press")
-    print("press esc to exit")
+def show_key_to_note():
+    print("\033c", end="")
+    # print("以下是所有键盘的键对应的音符:")
+    # for k, v in key_note.items():
+    #     if v != "":
+    #         print(f"key:{k} => note:{v}")
+    print("该程序启动后, 将该程序挂于后台, 便可与随心所欲的去敲代码或打字, 享受边敲边听的乐趣")
+    print("按下esc键可以退出子程序")
 
 
 def bind():
@@ -106,10 +110,8 @@ def bind():
         on_press=on_press, 
         on_release=on_release) as listener:
         listener.join()
-    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-    listener.start()
 
 
-def run():
-    show_in_terminal()
+def run_keymonitor():
+    show_key_to_note()
     bind()
