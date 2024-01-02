be2292a9d0a4c2db9d3e2c24e8dccfc70d856b06
handandkeep <1477792904@qq.com>
2024-01-02 19:31:50 +0800

    change import

--- keyplay/__init__.py
+++ keyplay/__init__.py
@@ -1,4 +1,4 @@
-from keyplay.bind import run
+from keyplay.run import run
 from keyplay.cplay import split_as_melody, split_as_chord, convert, listen_midi
 
 
@@ -7,6 +7,7 @@ create time: 2023-12-30
 author: Lns-XueFeng
 function: to play a rhythm when you code or typing and etc
 """
+
 
 __all__ = [
     "run", 
