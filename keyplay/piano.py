9dbd3d246e99f1b44141b0985baded88e24a66b2
handandkeep <1477792904@qq.com>
2024-01-02 19:34:49 +0800

    impl asciipiano

--- keyplay/piano.py
+++ keyplay/piano.py
@@ -1,4 +1,15 @@
-import colorama
+import numpy as np
+from pynput import keyboard
+from pynput.keyboard import Key
+
+from keyplay.play import play_note
+from keyplay.map import key_small_note as key_note
+
+
+"""piano.py
+这个模块可以在终端模拟一个25键的字符钢琴, 随着用户键盘对应键的按下而按下释放而释放, 
+当按下时便会播放对应的音符, 此模块目前还不支持多键同时按下, 后续会进行优化
+"""
 
 
 piano = """
@@ -20,5 +31,108 @@ piano = """
 └────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┘
 """
 
-# 转换为矩阵，通过操作矩阵实现不同的键被按下的效果
-# 增加键盘事件的监控，当按下对应的键时播放音符
+
+def show_piano(piano_matrix):
+    for r in piano_matrix:
+        for c in r:
+            print(c, end="")
+        print("")
+    print("按下esc键可以退出子程序")
+
+
+def gen_marix():
+    com_piano = []
+    for line in piano.split("\n"):
+        if len(line) == 0:
+            continue
+        splited_piano = []
+        for c in line:
+            splited_piano.append(c)
+        com_piano.append(splited_piano)
+    return np.array(com_piano)
+
+
+piano_matrix = gen_marix()
+print("\033c", end="")
+show_piano(piano_matrix)
+
+
+def change_pianowkey(start, end):
+    copy_piano_matrix = np.copy(piano_matrix)
+    index_row = 0
+    for r in piano_matrix:
+        index_col = start
+        for c in r[start:end]:
+            if c == " ":
+                copy_piano_matrix[index_row][index_col] = "█"
+            index_col = index_col + 1
+        index_row = index_row + 1
+    return copy_piano_matrix
+
+
+def change_pianobkey(start, end):
+    copy_piano_matrix = np.copy(piano_matrix)
+    index_row = 0
+    for r in piano_matrix[:8]:
+        index_col = start
+        for c in r[start:end]:
+            if c == "█":
+                copy_piano_matrix[index_row][index_col] = " "
+            index_col = index_col + 1
+        index_row = index_row + 1
+    return copy_piano_matrix
+
+
+def piano_keydown(key):
+    """ 模拟字符钢琴的key键被按下 """
+    wkey_dict = {"w": (0, 5), "e": (5, 10), "r": (10, 15), "t": (15, 20), 
+                "y": (20, 25), "u": (25, 30), "i": (30, 35), "s": (35, 40), 
+                "d": (40, 45), "f": (45, 50), "g": (50, 55), 
+                "h": (55, 60), "j": (60, 65), "k": (65, 70)}
+    for k, v in wkey_dict.items():
+        if key == k:
+            changed_piano_matrix = change_pianowkey(v[0], v[1])
+            show_piano(changed_piano_matrix)
+            print("\033c", end="")
+            play_note(key_note.get(key))
+    
+    bkey_dict = {"1": (3, 7), "2": (8, 12), "3": (18, 22), "4": (23, 27), 
+                "5": (28, 32), "6": (38, 42), "7": (43, 47), "8": (53, 57), 
+                "9": (58, 62), "0": (63, 67)}
+    for k, v in bkey_dict.items():
+        if key == k:
+            changed_piano_matrix = change_pianobkey(v[0], v[1])
+            show_piano(changed_piano_matrix)
+            print("\033c", end="")
+            play_note(key_note.get(key))
+
+
+def piano_keyup(key):
+    """ 模拟字符钢琴的key键被释放 """
+    show_piano(piano_matrix)
+    print("\033c", end="")
+
+
+def on_press(key):
+    try:
+        piano_keydown(key.char)
+    except AttributeError:
+        pass
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+    piano_keyup(key)
+
+
+def bind():
+    with keyboard.Listener(
+        on_press=on_press, 
+        on_release=on_release) as listener:
+        listener.join()
+
+
+def run_asciipiano():
+    print("\033c", end="")
+    bind()
